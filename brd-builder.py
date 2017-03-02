a9eec40954db9a393335a0dec09a350f5d1f1ad6
JarrettR <jrainier@gmail.com>
2017-03-01 23:49:54 -0800

    Added layer options

--- brd-builder.py
+++ brd-builder.py
@@ -5,9 +5,22 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 
 
-inFile = 'tests/pcb.Outline.bmp'
+inFile = '..\Top-solder.bmp'
 modelFile = 'blank.brd'
 outFile = 'out.brd'
+
+#Notable layers:
+#1:  Top
+#16: Bottom
+#21: tPlace (Top silkscreen)
+#22: bPlace (Bottom silkscreen)
+#29: tStop (Top solder)
+#30: bStop (Bottom solder)
+#31: tCream
+#32: bCream
+#39: tKeepout
+#40: bKeepout
+layer = '29'
 
 #Internal units are 0.1um, or 1/10000
 #So 1 pixel =
@@ -18,9 +31,13 @@ scale = 423
 
 class BmpToBrd(object):
 
-    def __init__(self, inFile, modelFile, outFile, scalingFactor = 1):
+    def __init__(self, inFile, modelFile, outFile, layer = '1', scalingFactor = 1):
         img = Image.open(inFile)
+        if img.mode is not '1':
+            print "Try again with Index Mode 1-bit colour!"
+            img.convert(mode='1')
         self.scalingFactor = scalingFactor / 10000.0
+        self.layer = layer
         self.componentElement = []
         
         self.Read_Lines(img)
@@ -47,7 +64,7 @@ class BmpToBrd(object):
         x2 = "{}".format(end * self.scalingFactor)
         y2 = "{}".format((y+1) * self.scalingFactor * -1)
         
-        self.componentElement.append(ET.Element('rectangle', attrib = {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'layer': '1'}))
+        self.componentElement.append(ET.Element('rectangle', attrib = {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'layer': self.layer}))
         
     def Update_Brd(self, modelFile):
         tree = ET.parse(modelFile)
@@ -69,4 +86,4 @@ class BmpToBrd(object):
         f.close()
 
 if __name__ == "__main__":
-    obj = BmpToBrd(inFile, modelFile, outFile, scale)
+    obj = BmpToBrd(inFile, modelFile, outFile, layer, scale)
